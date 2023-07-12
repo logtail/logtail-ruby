a15be777ee8b0c81df0e713c591e716d3980e170
Petr Heinz <petr@betterstack.com>
2023-07-12 14:29:54 +0200

    Fix TaggedLogging in Rails 7.0 (#19)

--- lib/logtail/logger.rb
+++ lib/logtail/logger.rb
@@ -57,6 +57,10 @@ module Logtail
 
         # Because of all the crazy ways Rails has attempted tags, we need this crazy method.
         def extract_active_support_tagged_logging_tags
+          if defined?(ActiveSupport::IsolatedExecutionState)
+            @current_tags ||= ActiveSupport::IsolatedExecutionState[tagged_logging_object_key_name]
+          end
+
           @current_tags ||
             Thread.current[:activesupport_tagged_logging_tags] ||
             Thread.current[tagged_logging_object_key_name] ||
